a4e13f044bdabbc45feca7e46b2755e6c58aefb1
andri <andriachmad93@gmail.com>
2022-01-14 21:38:38 +0700

    change help class desc

--- utils/help.rb
+++ utils/help.rb
@@ -4,11 +4,11 @@ class Help
     #command method to show helps quiz master cli command
     def self.command
         puts "Command | Description"
-        puts "create_question <no> <question> <answer> | Creates a question"
-        puts "update_question <no> <question> <answer> | Updates a question"
-        puts "delete_question <no> | Deletes a question"
-        puts "question <no> | Shows a question"
+        puts "create_question | Creates a question"
+        puts "update_question | Updates a question"
+        puts "delete_question | Deletes a question"
+        puts "question | Shows a question"
         puts "questions | Shows question list"
-        puts "answer_question <no> <answer> | Answers a question"
+        puts "answer_question | Answers a question"
     end
 end
